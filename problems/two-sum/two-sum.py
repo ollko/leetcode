8c04c1712cd4a73207e0e447ab2a4b2c8a1291b5
ollko <korotkaya.olga@yandex.ru>
2019-08-18 15:49:46 +0300

    Add solution with hash table

--- problems/two-sum/two-sum.py
+++ problems/two-sum/two-sum.py
@@ -1,10 +1,31 @@
 # Time complexity : O(n**2)
 # Space complexity : O(1)
-
-def twoSum(nums: List[int], target: int) -> List[int]:
+import time
+def twoSum(nums, target):
+    t0 = time.clock()
     i = 0
     j = 1
     for i in range(len(nums)-1):
         for j in range(i+1, len(nums)):
             if nums[i] + nums[j] == target:
+                t1 = time.clock()
+                print('twoSum', t1-t0)
                 return [i, j]
+
+
+def twoSum2(nums, target):
+    t0 = time.clock()
+    h_table = { nums[i] : i for i in range(len(nums)) }
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        if h_table.get(complement) and h_table[complement] != i:
+            t1 = time.clock()
+            print('twoSum1', t1-t0)
+            return [i, h_table[complement]]
+
+def test():
+    assert twoSum([2, 7, 11, 15], 9) == [0, 1]
+    assert twoSum2([2, 7, 11, 15], 9) == [0, 1]
+    return ("test pass")
+
+print(test())
