502532e26dc327a2c169f44563d58fddd7b93e4c
ollko <korotkaya.olga@yandex.ru>
2019-08-18 13:42:18 +0300

    Add solution with re

--- problems/string-to-integer-(atoi)/string-to-integer-(atoi).py
+++ problems/string-to-integer-(atoi)/string-to-integer-(atoi).py
@@ -28,3 +28,37 @@ def myAtoi(str):
             return 2**31 - 1
         else:
             return num*sign
+
+
+import re
+def myAtoi1(str):
+    """
+    :type str: str
+    :rtype: int
+    """
+    str = str.strip()
+    if not str:
+        return 0
+    else:
+        d = re.match(r'[+-]{0,1}\d+', str)
+        if not d:
+            return 0
+        else:
+            sign = 1
+            if str[0] in '+-':
+                if str[0] == '-':
+                    sign = -1
+                dstr = str[1:d.end()]
+            else:
+                dstr = str[:d.end()]
+
+            res = 0
+            for i in range(len(dstr)):
+                res += int(dstr[i]) * 10**((len(dstr)-1)-i)
+            if res*sign > 2**31 - 1:
+                return 2**31 - 1
+            elif res*sign < -2**31:
+                return -2**31
+            else:
+                return res*sign
+        
